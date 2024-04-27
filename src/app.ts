60255ab8ba26a83893c86f18d573acff417d4153
frohrlich <32771547+frohrlich@users.noreply.github.com>
2024-04-27 21:28:31 +0200

    Added start game screen

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import { BootScene } from "./scenes/BootScene";
 import { BattleScene } from "./scenes/BattleScene";
 import { BattleUIScene } from "./scenes/BattleUIScene";
 import { GameOverScene } from "./scenes/GameOverScene";
+import screenfull from "screenfull";
+import { UAParser } from "ua-parser-js";
 
 export const GAME_WIDTH = 930; // xiaomi mi a3 : 19.5:9 ratio
 export const GAME_HEIGHT = 416;
@@ -32,5 +34,35 @@ export class RpgGame extends Phaser.Game {
   }
 }
 window.onload = () => {
+  const parser = new UAParser();
+  const isNotDesktop =
+    parser.getDevice().type === "mobile" ||
+    parser.getDevice().type === "tablet";
+
+  const gameDiv = document.getElementById("game");
+  const fullscreenButton = document.getElementById("fullscreen-button");
+  const rotateScreenTip = document.getElementById("rotateScreenTip");
+  const fullscreenButtonDiv = document.getElementById("fullscreenButton-div");
+
+  if (isNotDesktop) rotateScreenTip.hidden = false;
+
+  fullscreenButton.addEventListener("click", () => {
+    gameDiv.hidden = false;
+    fullscreenButton.hidden = true;
+    fullscreenButtonDiv.style.display = "none";
+    if (isNotDesktop) {
+      if (screenfull.isEnabled) {
+        screenfull.request(gameDiv);
+        window.screen.orientation["lock"]("landscape");
+      } else {
+        alert("Error ! Please refresh your navigator.");
+      }
+    }
+  });
+
+  // gameDiv.hidden = false;
+  // fullscreenButton.hidden = true;
+  // fullscreenButtonDiv.style.display = "none";
+
   var game = new RpgGame(config);
 };
