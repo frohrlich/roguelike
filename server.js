75f5c0e83a65572f7867897d822859ef82469fae
frohrlich <32771547+frohrlich@users.noreply.github.com>
2024-05-06 00:54:19 +0200

    Heroku setup

---
+++ server.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+const HTML_FILE = path.join(__dirname, "public/index.html");
+
+app.use(express.static("./"));
+app.get("*", (req, res) => {
+  res.sendFile(HTML_FILE);
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
+});
