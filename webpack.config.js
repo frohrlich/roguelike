7beaecf22449de5758651808345b829c1b45628f
frohrlich <32771547+frohrlich@users.noreply.github.com>
2024-05-06 18:55:20 +0200

    Added local server config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   entry: "./src/app.ts",
   module: {
@@ -18,4 +20,18 @@ module.exports = {
     path: path.resolve(__dirname, "public/js"),
   },
   mode: "development",
+  devServer: {
+    static: {
+      directory: __dirname,
+    },
+    open: true,
+    compress: true,
+    port: 9000,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
 };
